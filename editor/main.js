f7dedab13c73afb3b887d703153c9f3a6223b96c
César Leblic <cesar.leblic@gmail.com>
2013-12-04 17:30:35 +0100

    Node webkit app controle Capture app

--- editor/main.js
+++ editor/main.js
@@ -1,6 +1,7 @@
 global.$ = $;
 
 var fs = require('fs')
+var exec = require('child_process').exec;
 
 $(document).ready(function() {
 
@@ -13,11 +14,35 @@ $(document).ready(function() {
     });
 
 
-    $(".btn-success").click(function(e){
-	e.preventDefault();
-	var j = form2js("form", "." , false, null, true);
-	fs.writeFile('./config.json', JSON.stringify(j, null, '\t'), function (err) {
-	    if (err) return console.log(err);
-	});
+    $(".save").click(function(e){
+		e.preventDefault();
+		var j = form2js("form", "." , false, null, true);
+		fs.writeFile('./../CAPTURE/config.json', JSON.stringify(j, null, '\t'), function (err) {
+	    	if (err) return console.log(err);
+		});
+		fs.writeFile('./config.json', JSON.stringify(j, null, '\t'), function (err) {
+	   		if (err) return console.log(err);
+		});
+    });
+
+    $(".save-and-launch").click(function(e){
+		e.preventDefault();
+		var j = form2js("form", "." , false, null, true);
+		fs.writeFile('./../CAPTURE/config.json', JSON.stringify(j, null, '\t'), function (err) {
+	    	if (err) return console.log(err);
+		});
+		fs.writeFile('./config.json', JSON.stringify(j, null, '\t'), function (err) {
+	   		if (err) return console.log(err);
+		});
+		exec('sh ../CAPTURE/launch', function callback(error, stdout, stderr){
+    	// result
+		});
+    });
+
+    $(".launch").click(function(e){
+		e.preventDefault();
+		exec('sh ../CAPTURE/launch', function callback(error, stdout, stderr){
+    	// result
+		});
     });
 });
